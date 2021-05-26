0315fa16114fa80a374007bbd17caa7aba31e0db
Brendan Clarke <clarkeb3@tcd.ie>
2021-05-26 17:17:04 +0100

    Tests updated to include self reprojection.

--- stixpy/vis/tests/test_map_reprojection.py
+++ stixpy/vis/tests/test_map_reprojection.py
@@ -13,6 +13,7 @@ files = Fido.fetch(res)
 # Convert using to SunPy map
 map = sunpy.map.Map(files)
 
+# Testing to check if SOLO position data are correct.
 def test_get_SOLO_Pos(map = map):
     aia_map = sunpy.map.Map(map)
     solo_pos = get_SOLO_Pos(aia_map)
@@ -25,4 +26,12 @@ def test_get_SOLO_Pos(map = map):
     assert check_y == True
     check_z = isinstance(solo_pos.data.xyz[2].value, np.float64)
     assert check_z == True
-    return(solo_pos)
+    return()
+
+# Testing to check if reprojection is correct by doing reprojecting map onto itself.
+def map_reproj_test(map=map, observer=map.observer_coordinate):
+    self_reprojected_map = reproject_map(map, observer)
+    assert np.allclose(map.observer_coordinate.lat, self_reprojected_map.observer_coordinate.lat)
+    assert np.allclose(map.observer_coordinate.lon, self_reprojected_map.observer_coordinate.lon)
+    assert np.allclose(map.observer_coordinate.radius, self_reprojected_map.observer_coordinate.radius)
+    return()

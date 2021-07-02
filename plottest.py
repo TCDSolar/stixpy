f915440fd144b0bbe3bd8d09a3a171a05111dbdf
Nicky Hochmuth <nicky.thecrag@gmail.com>
2021-07-02 11:04:23 +0200

    update plottest

--- plottest.py
+++ plottest.py
@@ -20,16 +20,18 @@ print(pd_file[0])
 # jsut printg the object gives a textual overview, pixel data support the same plot methods as
 # pd
 
-l1 = ScienceData.from_fits(test.STIX_SCI_XRAY_L1)
+#l1 = ScienceData.from_fits(test.STIX_SCI_XRAY_L1)
+l2 = ScienceData.from_fits(test.STIX_SCI_XRAY_L2)
+l2.plot_pixels()
 
-# spectrograms with an additional method to plot the pixel data default kind is pixel
-#pd.plot_pixels(energy_indices=[[1, 5], [5, 20], [20, 30]])
-#plt.show()
+# plot the pixel data as default "kind=pixels"
+pd.plot_pixels(energy_indices=[[1, 5], [5, 20], [20, 30]])
+plt.show()
 
-#pd.plot_pixels(kind='config')
-#plt.show()
-
+# plot the pixel data as line chart with errorbars"
 pd.plot_pixels(kind='errorbar', energy_indices=[[1, 5], [5, 20], [20, 30]])
 plt.show()
 
-print(1)
+# plot the detector configuration (independent on the actual pixeldata)"
+pd.plot_pixels(kind='config')
+plt.show()

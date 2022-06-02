dd6072b16d3bdf5635fa10974fea1a50999479f4
Shane Maloney <shane.maloney@tcd.ie>
2022-06-02 15:10:47 +0100

    Update for most recent stixcore and xrayvision

--- examlpes/imaging_demo.py
+++ examlpes/imaging_demo.py
@@ -1,12 +1,17 @@
+import logging
 import astropy.units as u
 import numpy as np
 
-from stixcore.calibration.visibility import calibrate_visibility, create_visibility
+from stixcore.calibration.visibility import calibrate_visibility, create_visibility, \
+    correct_phase_projection
 from stixpy.science import ScienceData
-from xrayvision.clean import clean_wrapper
+from xrayvision.clean import vis_clean
 from xrayvision.imaging import back_project
-from xrayvision.transform import idft_map
+from xrayvision.mem import mem
 from xrayvision.visibility import Visibility
+
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
 
 cpd = ScienceData.from_fits('/Users/shane/Downloads/solo_L1_stix-sci-xray-cpd_20200607T213709-20200607T215208_V01_1178428688-49155.fits')
 
@@ -14,51 +19,41 @@ time_range = ['2020-06-07T21:39:00', '2020-06-07T21:42:49']
 energy_range = [6, 10]
 flare_xy = [-1625, -700] * u.arcsec
 
-
+# TODO rename doesn't really create a complex vis just the real and imag parts
 vis = create_visibility(cpd, time_range=time_range, energy_range=energy_range, phase_center=flare_xy)
 
+# TODO rename
 cal_vis = calibrate_visibility(vis)
 
-# Phase projection correction
-L1 = 550 * u.mm
-L2 = 47 * u.mm
-# TOdo check how this works out to degress
-cal_vis.phase -= (flare_xy[1].to_value('arcsec') * 360. * np.pi
-                  / (180. * 3600. * 8.8*u.mm) * (L2 + L1 / 2.0))*u.deg
+cal_vis = correct_phase_projection(cal_vis, flare_xy)
 
-cal_vis.u *= -cal_vis.phase_sense
-cal_vis.v *= -cal_vis.phase_sense
-
-# Compute real and imaginary part of the visibility
-cal_vis.obsvis = cal_vis.amplitude * (np.cos(cal_vis.phase.to('rad')) + np.sin(cal_vis.phase.to('rad'))*1j)
-
-# Add phase factor for shifting the flare_xy
-map_center = flare_xy - [26.1, 58.2] * u.arcsec
-# Subtract Frederic's mean shift values
-
-# TODO check the u, v why is flare_xy[1] used with u (prob stix vs spacecraft orientation
-phase_mapcenter = -2 * np.pi * (map_center[1] * cal_vis.u - map_center[0] * cal_vis.v) * u.rad
-cal_vis.obsvis *= (np.cos(phase_mapcenter)+np.sin(phase_mapcenter) *1j)
-
+# order by sub-collimator e.g 10a, 10b, 10c, 9a, 9b, 9c ....
 isc_10_3 = [3, 20, 22, 16, 14, 32, 21, 26, 4, 24, 8, 28, 15, 27, 31, 6, 30, 2, 25, 5, 23, 7, 29, 1]
 idx = np.searchsorted(cal_vis.isc, isc_10_3)
 
 imsize = [129, 129]*u.pixel  # number of pixels of the map to reconstruct
-pixel = [2.,2.]*u.arcsec   # pixel size in aresec
+pixel = [2, 2]*u.arcsec   # pixel size in aresec
 
 stix_vis = Visibility(uv=np.vstack([cal_vis.u[idx], cal_vis.v[idx]]), vis=cal_vis.obsvis[idx],
                       xyoffset=flare_xy, pixel_size=pixel)
+skeys = stix_vis.__dict__.keys()
+for k, v in cal_vis.__dict__.items():
+    if k not in skeys:
+        setattr(stix_vis, k, v[idx])
 
 # back projection (inverse transform) natural weighting
-bp = back_project(stix_vis, imsize, pixel_size=pixel)
+bp_nat = back_project(stix_vis, imsize, pixel_size=pixel)
 
 # back project (inverse transform) uniform weighting
-
+bp_uni = back_project(stix_vis, imsize, pixel_size=pixel, natural=False)
 
 # Clean
 niter = 60  # number of iterations
 gain = 0.1  # gain used in each clean iteration
 beam_width = 20. * u.arcsec
-dirty_map = clean_wrapper(stix_vis, imsize, pixel=pixel,
-                          gain=gain, niter=niter, clean_beam_width=20*u.arcsec)
-print(dirty_map.shape)
+clean_map, model_map, resid_map = vis_clean(stix_vis, imsize, pixel=pixel, gain=gain, niter=niter,
+                                            clean_beam_width=20*u.arcsec)
+
+mem_map = mem(stix_vis, shape=imsize, pixel=pixel)
+
+print(mem_map)

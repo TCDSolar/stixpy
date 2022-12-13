5e57a844121480758caa6f6768b16f333f826ab2
Shane Maloney <shane.maloney@tcd.ie>
2022-12-13 10:11:27 +0000

    Fix code style errors

--- stixpy/data/test.py
+++ stixpy/data/test.py
@@ -14,24 +14,23 @@ def _unzip_test_data():
     data_tar = root_dir / 'test_data.tar.gz'
     with tarfile.open(data_tar.as_posix(), 'r:gz') as tar:
         def is_within_directory(directory, target):
-            
+
             abs_directory = os.path.abspath(directory)
             abs_target = os.path.abspath(target)
-        
+
             prefix = os.path.commonprefix([abs_directory, abs_target])
-            
+
             return prefix == abs_directory
-        
+
         def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-        
+
             for member in tar.getmembers():
                 member_path = os.path.join(path, member.name)
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
-        
-            tar.extractall(path, members, numeric_owner=numeric_owner) 
-            
-        
+
+            tar.extractall(path, members, numeric_owner=numeric_owner)
+
         safe_extract(tar, root_dir.absolute())
 
 

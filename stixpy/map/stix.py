a35a42ceee1f77cd017f7723c90d4a779aa5ccc1
Paolo Massa <64010598+paolomassa@users.noreply.github.com>
2024-05-03 23:16:12 +0200

    Feat calibration fixes (#106)
    Calibration fixes
    
    - Fixed ELUT correction
    - Implemented ELUT correction for count erros
    - Defined Energy Edge Masks
    - Implemented background subtraction in the demo
    
    ---------
    
    Co-authored-by: Shane Maloney <shane.maloney@dias.ie>

--- stixpy/map/stix.py
+++ stixpy/map/stix.py
@@ -17,14 +17,14 @@ class STIXMap(GenericMap):
 
         # Until this is resolved need to manually override https://github.com/astropy/astropy/issues/16339
         res = super().plot(**kwargs)
+        if self.coordinate_system == 'stiximaging':
+            res.axes.coords[0].set_coord_type('longitude', coord_wrap=180*u.deg)
+            res.axes.coords[1].set_coord_type('latitude')
 
-        res.axes.coords[0].set_coord_type('longitude', coord_wrap=180*u.deg)
-        res.axes.coords[1].set_coord_type('latitude')
+            res.axes.coords[0].set_format_unit(u.arcsec)
+            res.axes.coords[1].set_format_unit(u.arcsec)
 
-        res.axes.coords[0].set_format_unit(u.arcsec)
-        res.axes.coords[1].set_format_unit(u.arcsec)
-
-        res.axes.coords[0].set_axislabel(r'STIX $\Theta_{X}$')
-        res.axes.coords[1].set_axislabel(r'STIX $\Theta_{Y}$')
+            res.axes.coords[0].set_axislabel(r'STIX $\Theta_{X}$')
+            res.axes.coords[1].set_axislabel(r'STIX $\Theta_{Y}$')
 
         return res

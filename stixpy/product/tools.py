2f8434838d529ec93e5f5a62a93033e25f40a46d
DanRyanIrish <ryand5@tcd.ie>
2024-05-03 16:05:52 +0100

    Extend rebin_irregular() to propagate uncertainties.

--- stixpy/product/tools.py
+++ stixpy/product/tools.py
@@ -2,7 +2,9 @@
 Analysis tools.
 """
 
+import astropy.nddata
 import numpy as np
+from ndcube.utils.cube import propagate_rebin_uncertainties
 
 ARRAY_MASK_MAP = {}
 ARRAY_MASK_MAP[np.ndarray] = np.ma.masked_array
@@ -28,7 +30,7 @@ def _convert_to_masked_array(data, mask, operation_ignores_mask):
 
 
 def rebin_irregular(cube, axes_idx_edges, operation=np.mean, operation_ignores_mask=False,
-                    handle_mask=np.all):
+                    handle_mask=np.all, propagate_uncertainties=False, new_unit=None, **kwargs):
     """
     Downsample array by combining irregularly sized, but contiguous, blocks of elements into bins.
 
@@ -66,15 +68,41 @@ def rebin_irregular(cube, axes_idx_edges, operation=np.mean, operation_ignores_m
     ndim = len(cube.shape)
     if len(axes_idx_edges) != ndim:
         raise ValueError(f"length of axes_idx_edges must be {ndim}")
+
     # Combine data and mask and determine whether mask also needs rebinning.
-    data = _convert_to_masked_array(cube.data, cube.mask, operation_ignores_mask)
+    new_data = _convert_to_masked_array(cube.data, cube.mask, operation_ignores_mask)
     rebin_mask = False
     if handle_mask is None or operation_ignores_mask:
-        mask = None
+        new_mask = None
     else:
-        mask = cube.mask
+        new_mask = cube.mask
         if not isinstance(cube.mask, (type(None), bool)):
             rebin_mask = True
+
+    # Determine whether to propagate uncertainties
+    if propagate_uncertainties:
+        # Temporarily set propagate_uncertainties to False and then determine whether they
+        # can/should be set back to True.
+        propagate_uncertainties = False
+        if cube.uncertainty is None:
+            warn_user("Uncertainties cannot be propagated as there are no uncertainties, "
+                          "i.e., the `uncertainty` keyword was never set on creation of this NDCube.")
+        elif isinstance(cube.uncertainty, astropy.nddata.UnknownUncertainty):
+            warn_user("The uncertainty on this NDCube has no known way to propagate forward and so will be dropped. "
+                          "To create an uncertainty that can propagate, please see "
+                          "https://docs.astropy.org/en/stable/uncertainty/index.html")
+        elif (not operation_ignores_mask
+              and (cube.mask is True or (cube.mask is not None
+                                         and not isinstance(cube.mask, bool)
+                                         and cube.mask.all()))):
+            warn_user("Uncertainties cannot be propagated as all values are masked and "
+                      "operation_ignores_mask is False.")
+        else:
+            propagate_uncertainties = True  # Uncertainties can be propagated. Reset to True.
+            new_uncertainty = cube.uncertainty
+    else:
+        new_uncertainty = None
+
     # Iterate through dimensions and perform rebinning operation.
     for i, edges in enumerate(axes_idx_edges):
         # If no edge indices provided for dimension, skip to next one.
@@ -83,14 +111,32 @@ def rebin_irregular(cube, axes_idx_edges, operation=np.mean, operation_ignores_m
         # Iterate through pixel blocks and collapse via rebinning operation.
         tmp_data = []
         tmp_mask = []
+        tmp_uncertainties = []
         item = [slice(None)] * ndim
         for j in range(len(edges)-1):
+            # Rebin data
             item[i] = slice(edges[j], edges[j+1])
-            tmp_data.append(operation(data[*item], axis=i))
+            data_chunk = new_data[*item]
+            tmp_data.append(operation(data_chunk, axis=i))
+            # Rebin mask
             if rebin_mask is True:
-                tmp_mask.append(handle_mask(mask[*item], axis=i))
-        data = np.stack(tmp_data, axis=i)
+                mask_chunk = new_mask[*item]
+                tmp_mask.append(handle_mask(mask_chunk, axis=i))
+            # Rebin uncertainties
+            if propagate_uncertainties:
+                # Reorder axis i to be 0th axis as this is format required by propagate_rebin_uncertainties
+                uncertainty_chunk = type(new_uncertainty)(np.moveaxis(new_uncertainty.array[*item], i, 0), unit=new_uncertainty.unit)
+                data_unc = np.moveaxis(data_chunk, i, 0)
+                mask_unc = np.moveaxis(mask_chunk, i, 0) if rebin_mask else mask
+                tmp_uncertainty = propagate_rebin_uncertainties(uncertainty_chunk, data_unc, mask_unc, operation,
+                                                                operation_ignores_mask=operation_ignores_mask,
+                                                                handle_mask=handle_mask, new_unit=new_unit, **kwargs)
+                tmp_uncertainties.append(tmp_uncertainty.array)
+        # Restore rebinned axes.
         if rebin_mask:
-            mask = np.stack(tmp_mask, axis=i)
-    data = data.data
-    return data, mask
+            new_mask = np.stack(tmp_mask, axis=i)
+        new_data = np.ma.masked_array(np.stack(tmp_data, axis=i).data, mask=new_mask)
+        if propagate_uncertainties:
+            new_uncertainty = type(new_uncertainty)(np.stack(tmp_uncertainties, axis=i),unit=new_uncertainty.unit)
+
+    return new_data.data, new_mask, new_uncertainty

ba3b71f32e4d2393896718afe875e65f2b2bca7b
Shane Maloney <shane.maloney@dias.ie>
2024-05-17 13:06:29 +0100

    Bugifx ql timeseries units (#118)
    * Fix wrong units being set on QL timeseries
    * Update plot code to use more of sunpy TimeSeries functions
    * Add columns keyword with useful defaults
    * Remove redundant code fix columns for non default value.
    * Make sure peek works as expected
    * Better tests, create QTables from hdu and get units
    * Fix strange bug on python3.9
    * Replace live time correction with more accurate version
    * Code review and update HK timeseries in the same ways as QL
    ---------
    
    Co-authored-by: DanRyanIrish <ryand5@tcd.ie>
    Co-authored-by: Laura Hayes <lauraannhayes@gmail.com>

--- stixpy/timeseries/tests/test_quicklook.py
+++ stixpy/timeseries/tests/test_quicklook.py
@@ -1,3 +1,4 @@
+import astropy.units as u
 import pytest
 from sunpy.timeseries import TimeSeries
 
@@ -5,32 +6,87 @@ from stixpy.timeseries.quicklook import HKMaxi, QLBackground, QLLightCurve, QLVa
 
 
 @pytest.mark.remote_data
-def test_ql_lightcurve():
+@pytest.fixture()
+def ql_lightcurve():
     ql_lc = TimeSeries(
         r"https://pub099.cs.technik.fhnw.ch/fits/L1/2020/05/06/QL/solo_L1_stix-ql-lightcurve_20200506_V02.fits"
     )
-    assert isinstance(ql_lc, QLLightCurve)
+    return ql_lc
 
 
 @pytest.mark.remote_data
-def test_qlbackground():
-    ql_lc = TimeSeries(
+@pytest.fixture()
+def ql_background():
+    ql_bkg = TimeSeries(
         r"https://pub099.cs.technik.fhnw.ch/fits/L1/2020/05/06/QL/solo_L1_stix-ql-background_20200506_V02.fits"
     )
-    assert isinstance(ql_lc, QLBackground)
+    return ql_bkg
 
 
 @pytest.mark.remote_data
-def test_qlvariance():
-    ql_lc = TimeSeries(
+@pytest.fixture()
+def ql_variance():
+    ql_var = TimeSeries(
         r"https://pub099.cs.technik.fhnw.ch/fits/L1/2020/05/06/QL/solo_L1_stix-ql-variance_20200506_V02.fits"
     )
-    assert isinstance(ql_lc, QLVariance)
+    return ql_var
+
+
+# @pytest.mark.remote_data
+# @pytest.fixture()
+# def hk_maxi():
+#     hk_maxi = TimeSeries(
+#         r"https://pub099.cs.technik.fhnw.ch/fits/L1/2020/05/06/HK/solo_L1_stix-hk-maxi_20200506_V02U.fits"
+#     )
+#     return hk_maxi
+
+
+@pytest.mark.remote_data
+def test_ql_lightcurve(ql_lightcurve):
+    assert isinstance(ql_lightcurve, QLLightCurve)
+    assert ql_lightcurve.quantity('4-10 keV').unit == u.ct/(u.s * u.keV)
+
+
+@pytest.mark.remote_data
+def test_ql_lightcurve_plot(ql_lightcurve):
+    ql_lightcurve.plot()
+    ql_lightcurve.plot(columns=['4-10 keV'])
+
+
+@pytest.mark.remote_data
+def test_ql_background(ql_background):
+    assert isinstance(ql_background, QLBackground)
+    assert ql_background.quantity('4-10 keV').unit == u.ct / (u.s * u.keV)
+
+
+@pytest.mark.remote_data
+def test_ql_background_plot(ql_background):
+    ql_background.plot()
+    ql_background.plot(columns=['4-10 keV'])
+
+
+@pytest.mark.remote_data
+def test_ql_variance(ql_variance):
+    assert isinstance(ql_variance, QLVariance)
+    assert ql_variance.quantity('4-20 keV').unit == u.ct / (u.s * u.keV)
+
+
+@pytest.mark.remote_data
+def test_ql_variance_plot(ql_variance):
+    ql_variance.plot()
+    ql_variance.plot(columns=['4-20 keV'])
 
 
 @pytest.mark.remote_data
 def test_hk_maxi():
     hk_maxi = TimeSeries(
-        "https://pub099.cs.technik.fhnw.ch/fits/L1/2020/05/06/HK/solo_L1_stix-hk-maxi_20200506_V02U.fits"
-    )
+         "https://pub099.cs.technik.fhnw.ch/fits/L1/2020/05/06/HK/solo_L1_stix-hk-maxi_20200506_V02U.fits"
+     )
     assert isinstance(hk_maxi, HKMaxi)
+    assert hk_maxi.quantity('hk_att_c').unit == u.mA
+
+
+# @pytest.mark.remote_data
+# def test_hk_maxi_plot(hk_maxi):
+#     hk_maxi.plot()
+#     hk_maxi.plot(columns=['hk_asp_photoa0_v'])

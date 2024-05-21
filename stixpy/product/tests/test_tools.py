63d6b008d23f92e696ba4f8eb3baea0c73c2e45c
DanRyanIrish <ryand5@tcd.ie>
2024-05-21 10:27:48 +0200

    Update irregular rebin test to check output WCS.

--- stixpy/product/tests/test_tools.py
+++ stixpy/product/tests/test_tools.py
@@ -7,6 +7,7 @@ from astropy.tests.helper import assert_quantity_allclose
 from astropy.time import Time
 from ndcube import ExtraCoords, NDCube
 from ndcube.extra_coords import QuantityTableCoordinate, TimeTableCoordinate
+from ndcube.tests.helpers import assert_cubes_equal
 
 from .. import tools
 
@@ -31,21 +32,27 @@ def cube():
 def test_rebin_irregular(cube):
     # Run function.
     axes_idx_edges = [0, 2], [0, 1, 3, 5]
-    data, mask, uncertainty, coords = tools.rebin_irregular(cube, axes_idx_edges, operation=np.sum,
-                                                            operation_ignores_mask=False,
-                                                            handle_mask=np.all,
-                                                            propagate_uncertainties=True)
+    output = tools.rebin_irregular(cube, axes_idx_edges, operation=np.sum,
+                                   operation_ignores_mask=False, handle_mask=np.all,
+                                   propagate_uncertainties=True)
     # Define expected outputs.
-    expected_data = np.array([[1., 2., 2.]])
-    expected_mask = np.zeros(expected_data.shape, dtype=bool)
-    expected_uncertainty = astropy.nddata.StdDevUncertainty([[0.1,
-                                                              0.14142135623730953,
-                                                              0.14142135623730953]])
-    expected_coords = [[5.] * u.s, [3., 4.5, 6.5] * u.keV]
+    data = np.array([[1., 2., 2.]])
+    mask = np.zeros(data.shape, dtype=bool)
+    uncertainty = astropy.nddata.StdDevUncertainty([[0.1,
+                                                     0.14142135623730953,
+                                                     0.14142135623730953]])
+    ec = ExtraCoords()
+    ec.add('iso(utc; None', 1, [5.] * u.s, physical_types="time")
+    ec.add("energy", 0, [3., 4.5, 6.5] * u.keV, physical_types="em.energy")
+    wcs = ec.wcs
+    expected = NDCube(data, wcs, uncertainty=uncertainty, mask=mask)
     # Compare outputs with expected.
+    assert_cubes_equal(output, expected)
+    """
     np.testing.assert_allclose(data, expected_data)
     assert (mask == expected_mask).all()
     assert isinstance(uncertainty, type(expected_uncertainty))
     np.testing.assert_allclose(uncertainty.array, expected_uncertainty.array)
     assert_quantity_allclose(coords[0], expected_coords[0])
     assert_quantity_allclose(coords[1], expected_coords[1])
+    """

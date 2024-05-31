54d4e0794627763335bca9b9f2b520bef1ae2c69
paolomassa <paolocrmassa@gmail.com>
2024-05-31 21:44:34 +0200

    Fix pre-commit.

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ intersphinx_mapping = {
     "matplotlib": ("https://matplotlib.org/", (None, "http://data.astropy.org/intersphinx/matplotlib.inv")),
     "astropy": ("http://docs.astropy.org/en/stable/", None),
     "sunpy": ("https://docs.sunpy.org/en/stable/", None),
-    "xrayvision": ("https://xrayvision.readthedocs.io/en/stable/", None)
+    "xrayvision": ("https://xrayvision.readthedocs.io/en/stable/", None),
 }
 
 # -- Options for HTML output -------------------------------------------------

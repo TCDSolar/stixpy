58866e90797a2535a18d37b806370454a795ae4e
Shane Maloney <shane.maloney@dias.ie>
2024-06-04 08:57:56 +0200

    Add more QL products and add useful properties to QLFlareFlag

--- stixpy/product/sources/quicklook.py
+++ stixpy/product/sources/quicklook.py
@@ -1,8 +1,17 @@
+import astropy.units as u
+import numpy as np
+from astropy.coordinates import SkyCoord
+from numpy.typing import NDArray
 from sunpy.time import TimeRange
 
+from stixpy.coordinates.frames import STIXImaging
 from stixpy.product.product import L1Product
 
-__all__ = ["QuickLookProduct", "QLLightCurve", "QLBackground"]
+THERMAL_INDEX_MAP = np.array(["None", "Minor", "Small", "Moderate", "Major"])
+FLUX_INDEX_MAP = np.array(["None", "Weak", "Significant"])
+LOCATION_INDEX_MAP = np.array(["None", "Previous", "New"])
+
+__all__ = ["QuickLookProduct", "QLLightCurve", "QLBackground", "QLFlareFlag"]
 
 
 class QuickLookProduct(L1Product):
@@ -54,6 +63,23 @@ class QLBackground(QuickLookProduct):
         return f"{self.__class__.__name__}\n" f"    {self.time_range}"
 
 
+class QLVariance(QuickLookProduct):
+    """
+    Quicklook variance nominally in 5 energy bins every 8s
+    """
+
+    @classmethod
+    def is_datasource_for(cls, *, meta, **kwargs):
+        """Determines if meta data meach Raw Pixel Data"""
+        service_subservice_ssid = tuple(meta[name] for name in ["STYPE", "SSTYPE", "SSID"])
+        level = meta["level"]
+        if service_subservice_ssid == (21, 6, 33) and level == "L1":
+            return True
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}\n" f"    {self.time_range}"
+
+
 class QLFlareFlag(QuickLookProduct):
     """
     Quicklook Flare flag and location
@@ -65,6 +91,53 @@ class QLFlareFlag(QuickLookProduct):
         service_subservice_ssid = tuple(meta[name] for name in ["STYPE", "SSTYPE", "SSID"])
         level = meta["level"]
         if service_subservice_ssid == (21, 6, 34) and level == "L1":
+            return True
+
+    @property
+    def thermal_index(self):
+        r"""
+        Flare thermal index - amount of thermal emission
+        """
+        return THERMAL_INDEX_MAP[self.data["thermal_index"]]
+
+    @property
+    def flare_location(self) -> SkyCoord:
+        r"""
+        Flare location
+        """
+        return SkyCoord(
+            self.data["loc_y"] * u.arcsec, -self.data["loc_z"] * u.arcsec, frame=STIXImaging(obstie=self.data["time"])
+        )
+
+    @property
+    def non_thermal_index(self) -> NDArray[str]:
+        r"""
+        Flare non-thermal index - significant non-thermal emission
+        """
+        return THERMAL_INDEX_MAP[self.data["thermal_index"]]
+
+    @property
+    def location_flag(self) -> NDArray[str]:
+        r"""
+        Flare location flag
+        """
+        return THERMAL_INDEX_MAP[self.data["thermal_index"]]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}\n" f"    {self.time_range}"
+
+
+class QLTMStatusFlareList(QuickLookProduct):
+    """
+    Quicklook variance nominally in 5 energy bins every 8s
+    """
+
+    @classmethod
+    def is_datasource_for(cls, *, meta, **kwargs):
+        """Determines if meta data meach Raw Pixel Data"""
+        service_subservice_ssid = tuple(meta[name] for name in ["STYPE", "SSTYPE", "SSID"])
+        level = meta["level"]
+        if service_subservice_ssid == (21, 6, 43) and level == "L1":
             return True
 
     def __repr__(self):

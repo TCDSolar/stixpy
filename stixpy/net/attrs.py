e3d8d61763bcc5ff1fc84bc2c9dbe1764f382e6a
Nicky Hochmuth <nicky.thecrag@gmail.com>
2025-02-10 16:41:40 +0100

    Add a option for local search to fido client (#143)
    * Add a option for local search to fido client
    
    * Add Version attr to FIDO search
    
    * Change Version to proper attr and LatestVersion to a QueryResponse object method
    
    ---------
    
    Co-authored-by: nicky.hochmuth <nicky.hochmuth@ateleris.ch>
    Co-authored-by: Albert Y. Shih <ayshih@gmail.com>
    Co-authored-by: Shane Maloney <shane.maloney@dias.ie>

--- stixpy/net/attrs.py
+++ stixpy/net/attrs.py
@@ -1,3 +1,5 @@
+import re
+
 from sunpy.net.attr import SimpleAttr
 
 __all__ = ["DataType", "DataProduct"]
@@ -7,6 +9,82 @@ class DataType(SimpleAttr):
     """
     Data type Quicklook (QL), science (SCI), or housekeeping (HK)
     """
+
+
+class Version(SimpleAttr):
+    """
+    Exact version of the data file
+    """
+
+    PATTERN = re.compile(r"V(\d{2})([a-zA-Z]?)")
+
+    def __init__(self, version: int):
+        super().__init__(version)
+        self.allow_uncompleted = False
+        self.operator = int.__eq__
+
+    def matches(self, ver: str) -> bool:
+        match = Version.PATTERN.match(ver)
+        if match is None:
+            return False
+        v = int(match.group(1))
+        u = match.group(2)
+
+        ver_res = self.operator(v, self.value)
+        u_res = u in ["", "U"] if self.allow_uncompleted else u == ""
+
+        return ver_res and u_res
+
+
+class VersionU(Version):
+    """
+    min version of the data file
+    """
+
+    def __init__(self, version: int):
+        super().__init__(version)
+        self.allow_uncompleted = True
+        self.operator = int.__eq__
+
+
+class MinVersion(Version):
+    """
+    min version of the data file
+    """
+
+    def __init__(self, version: int):
+        super().__init__(version)
+        self.operator = int.__ge__
+
+
+class MinVersionU(VersionU):
+    """
+    min version of the data file
+    """
+
+    def __init__(self, version: int):
+        super().__init__(version)
+        self.operator = int.__ge__
+
+
+class MaxVersion(Version):
+    """
+    max version of the data file
+    """
+
+    def __init__(self, version: int):
+        super().__init__(version)
+        self.operator = int.__lt__
+
+
+class MaxVersionU(VersionU):
+    """
+    max version of the data file
+    """
+
+    def __init__(self, version: int):
+        super().__init__(version)
+        self.operator = int.__lt__
 
 
 class DataProduct(SimpleAttr):
--- stixpy/net/tests/test_client.py
+++ stixpy/net/tests/test_client.py
@@ -16,6 +16,45 @@ def client():
 
 
 @pytest.fixture
+def clientlocal():
+    """
+    TestData dir contains the following files and directories.
+    The files are empty but the structure and names are used to test the client.
+    .
+    ├── ANC
+    │   └── 2022
+    │       └── 01
+    │           ├── 01
+    │           │   └── ASP
+    │           │       └── solo_ANC_stix-asp-ephemeris_20220101_V02.fits
+    │           ├── 02
+    │           │   └── ASP
+    │           │       └── solo_ANC_stix-asp-ephemeris_20220102_V02.fits
+    │           └── 03
+    │               └── ASP
+    │                   └── solo_ANC_stix-asp-ephemeris_20220103_V02.fits
+    ├── L1
+    │   └── 2022
+    │       └── 01
+    │           └── 01
+    │               ├── QL
+    │               │   ├── solo_L1_stix-ql-lightcurve_20220101_V02.fits
+    │               │   ├── solo_L1_stix-ql-lightcurve_20220101_V05.fits
+    │               │   ├── solo_L1_stix-ql-lightcurve_20220101_V06U.fits
+    │               │   └── solo_L1_stix-ql-spectra_20220101_V02.fits
+    │               └── SCI
+    │                   ├── solo_L1_stix-sci-xray-cpd_20220101T003000-20220101T013000_V01U_2201010007-54548.fits
+    │                   ├── solo_L1_stix-sci-xray-cpd_20220101T003000-20220101T013000_V02_2201010007-54548.fits
+    │                   ├── solo_L1_stix-sci-xray-cpd_20220101T003000-20220101T013000_V03U_2201010007-54548.fits
+    │                   ├── solo_L1_stix-sci-xray-cpd_20220101T010000-20220101T020000_V01_2201010007-54549.fits
+    │                   ├── solo_L1_stix-sci-xray-cpd_20220101T013000-20220101T023000_V04_2201010007-54550.fits
+    │                   └── solo_L1_stix-sci-xray-cpd_20220101T020000-20220101T030000_V01_2201010007-54551.fits
+    └── test.html
+    """
+    return STIXClient(source=f"file://{Path(__file__).parent / 'data'}")
+
+
+@pytest.fixture
 def http_response():
     path = Path(__file__).parent / "data" / "test.html"
     with path.open("r") as f:
@@ -33,6 +72,7 @@ def http_response():
         (("2022-01-01T00:35:00", "2022-01-01T00:45:00"), 1),
         (("2022-01-01T00:00:00", "2022-01-01T00:35:00"), 1),
         (("2022-01-01T02:45:00", "2022-01-01T05:00:00"), 1),
+        (("2023-01-01T02:45:00", "2023-01-01T05:00:00"), 0),
     ],
 )
 @mock.patch(MOCK_PATH)
@@ -44,10 +84,117 @@ def test_client(urlopen, client, http_response, time_range, nfiles):
     assert len(query) == nfiles
 
 
+@pytest.mark.parametrize(
+    "time_range, level, dtype, nfiles",
+    [
+        (("2022-01-01T00:00:00", "2022-01-01T03:00:00"), "L1", a.stix.DataType.sci, 6),
+        (("2022-01-01T00:00:00", "2022-01-01T01:00:00"), "L1", a.stix.DataType.sci, 4),
+        (("2022-01-01T00:30:00", "2022-01-01T01:00:00"), "L1", a.stix.DataType.sci, 4),
+        (("2022-01-01T00:35:00", "2022-01-01T00:45:00"), "L1", a.stix.DataType.sci, 3),
+        (("2022-01-01T00:00:00", "2022-01-01T00:35:00"), "L1", a.stix.DataType.sci, 3),
+        (("2022-01-01T02:45:00", "2022-01-01T05:00:00"), "L1", a.stix.DataType.sci, 1),
+        (("2023-01-01T02:45:00", "2023-01-01T05:00:00"), "L1", a.stix.DataType.sci, 0),
+        (("2022-01-01T00:00:00", "2022-01-01T03:00:00"), "L1", a.stix.DataType.ql, 4),
+        (("2022-01-01T00:00:00", "2022-01-01T03:00:00"), "ANC", a.stix.DataType.asp, 1),
+        (("2022-01-01T00:00:00", "2022-01-02T03:00:00"), "ANC", a.stix.DataType.asp, 2),
+        (("2022-01-01T00:00:00", "2022-01-03T03:00:00"), "ANC", a.stix.DataType.asp, 3),
+        (("2022-01-01T00:00:00", "2022-01-05T03:00:00"), "ANC", a.stix.DataType.asp, 3),
+    ],
+)
+def test_local_client(clientlocal, time_range, level, dtype, nfiles):
+    query = clientlocal.search(
+        a.Time(*time_range),
+        a.Instrument.stix,
+        a.Level(level),
+        dtype,
+    )
+    assert len(query) == nfiles
+
+
 @pytest.mark.remote_data
 def test_search_date(client):
     res = client.search(a.Time("2020-05-01T00:00", "2020-05-01T23:59"), a.Instrument.stix)
-    assert len(res) == 38
+    assert len(res) == 39
+    # this might need fixed when we change to ANC to become an level of its own
+
+
+def test_search_max_version(clientlocal):
+    res = clientlocal.search(a.Time("2022-01-01T00:00", "2022-01-01T23:59"), a.Instrument.stix, a.stix.MaxVersion(3))
+    assert len(res) == 7
+
+    res = clientlocal.search(
+        a.Time("2022-01-01T00:00", "2022-01-01T23:59"),
+        a.Instrument.stix,
+        a.stix.MaxVersionU(3),
+    )
+    assert len(res) == 8
+
+
+def test_search_min_version(clientlocal):
+    res = clientlocal.search(a.Time("2022-01-01T00:00", "2022-01-01T23:59"), a.Instrument.stix, a.stix.MinVersion(2))
+    assert len(res) == 7
+
+    res = clientlocal.search(
+        a.Time("2022-01-01T00:00", "2022-01-01T23:59"),
+        a.Instrument.stix,
+        a.stix.MinVersionU(2),
+    )
+    assert len(res) == 9
+
+
+def test_search_version(clientlocal):
+    res = clientlocal.search(a.Time("2022-01-01T00:00", "2022-01-01T23:59"), a.Instrument.stix, a.stix.Version(2))
+    assert len(res) == 5
+
+    res = clientlocal.search(
+        a.Time("2022-01-01T00:00", "2022-01-01T23:59"),
+        a.Instrument.stix,
+        a.stix.VersionU(1),
+    )
+    assert len(res) == 3
+
+
+def test_search_version_and(clientlocal):
+    res = clientlocal.search(
+        a.Time("2022-01-01T00:00", "2022-01-01T23:59"), a.Instrument.stix, a.stix.MinVersion(2), a.stix.MaxVersionU(5)
+    )
+    assert len(res) == 6
+
+
+def test_search_latest_version(clientlocal):
+    res = clientlocal.search(a.Time("2022-01-01T00:00", "2022-01-01T23:59"), a.Instrument.stix)
+    res.filter_for_latest_version(allow_uncompleted=True)
+    assert len(res) == 7
+
+    res = clientlocal.search(
+        a.Time("2022-01-01T00:00", "2022-01-01T23:59"),
+        a.Instrument.stix,
+    )
+    res.filter_for_latest_version(allow_uncompleted=False)
+    assert len(res) == 7
+
+    res = clientlocal.search(a.Time("2022-01-01T00:00", "2022-01-01T23:59"), a.Instrument.stix, a.stix.DataType.ql)
+    res.filter_for_latest_version(allow_uncompleted=False)
+    res.sort("DataProduct")
+
+    assert len(res) == 2
+    assert res["DataProduct"][0] == "ql-lightcurve"
+    assert res["Ver"][0] == "V05"
+    assert res["DataProduct"][1] == "ql-spectra"
+    assert res["Ver"][1] == "V02"
+
+    res = clientlocal.search(
+        a.Time("2022-01-01T00:00", "2022-01-01T23:59"),
+        a.Instrument.stix,
+        a.stix.DataType.ql,
+    )
+    res.filter_for_latest_version(allow_uncompleted=True)
+    res.sort("DataProduct")
+    assert len(res) == 2
+    assert res["DataProduct"][0] == "ql-lightcurve"
+    assert res["Ver"][0] == "V06U"
+    assert res["DataProduct"][1] == "ql-spectra"
+    assert res["Ver"][1] == "V02"
 
 
 @pytest.mark.remote_data
@@ -86,7 +233,8 @@ def test_search_date_product_sci():
 @pytest.mark.remote_data
 def test_fido():
     res = Fido.search(a.Time("2020-11-17T00:00", "2020-11-17T23:59"), a.Instrument.stix)
-    assert len(res["stix"]) == 51
+    assert len(res["stix"]) == 52
+    # this might need fixed when we change to ANC to become an level of its own
 
     res_ql = Fido.search(a.Time("2020-11-17T00:00", "2020-11-17T23:59"), a.Instrument.stix, a.stix.DataType.ql)
     assert len(res_ql["stix"]) == 6
